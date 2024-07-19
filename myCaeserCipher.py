418f671d7e95e785f0c4359de89a6c637ff7506b
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-07-19 22:28:46 +0100

    Update myCaeserCipher.py

--- myCaeserCipher.py
+++ myCaeserCipher.py
@@ -2,7 +2,7 @@ alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
 'n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m',
 'n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-def ceaser_cipher(text,shift,direction):
+def caeser_cipher(text,shift,direction):
     word = ""
     if direction == 'decode':
         shift *= -1
