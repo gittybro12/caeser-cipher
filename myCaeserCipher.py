52fed70cb623ac77fde58d11e75c0f7172a0935f
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-07-26 20:43:28 +0100

    Update myCaeserCipher.py

--- myCaeserCipher.py
+++ myCaeserCipher.py
@@ -1,7 +1,6 @@
 alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
 'n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m',
 'n','o','p','q','r','s','t','u','v','w','x','y','z']
-
 def caeser_cipher(text,shift,direction):
     word = ""
     if direction == 'decode':
@@ -15,16 +14,12 @@ def caeser_cipher(text,shift,direction):
         else:
             word += text  
     print(f"here is your {direction}d result: {word}")          
-
 play= True
 while play:
     direction = input("Enter 'encode' to encrypt and 'decode' to decrypt\n").lower()
     text = input("Enter the word here\n")
     shift = int(input("Enter the number of shift\n"))
-
-
     ceaser_cipher(text,shift,direction)
-
     play_again = input("Enter 'yes' to play again and 'no' to quit\n" ).lower()
 
     if play_again == 'no':
